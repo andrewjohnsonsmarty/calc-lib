bf87c787d1618d0a8385322976071c9583e57e64
Andrew Johnson <andrewjohnson0613@gmail.com>
2024-11-05 10:24:49 -0700

    Added Subtraction, Multiplication, and Divison.

--- calc.go
+++ calc.go
@@ -4,8 +4,22 @@ type Calculator interface {
 	Calculate(a, b int) int
 }
 
-type Addition struct{}
+type (
+	Addition       struct{}
+	Subtraction    struct{}
+	Multiplication struct{}
+	Division       struct{}
+)
 
 func (this *Addition) Calculate(a, b int) int {
 	return a + b
+}
+func (this *Subtraction) Calculate(a, b int) int {
+	return a - b
+}
+func (this *Multiplication) Calculate(a, b int) int {
+	return a * b
+}
+func (this *Division) Calculate(a, b int) int {
+	return a / b
 }
--- calc_test.go
+++ calc_test.go
@@ -10,7 +10,9 @@ func TestAddition_Calculate(t *testing.T) {
 		a, b, want int
 	}{
 		{a: 0, b: 0, want: 0},
+		{a: 0, b: 1, want: 1},
 		{a: 1, b: 1, want: 2},
+		{a: 5, b: 3, want: 8},
 	}
 	for _, tt := range tests {
 		t.Run(fmt.Sprintf("%d + %d = %d", tt.a, tt.b, tt.want), func(t *testing.T) {
@@ -21,4 +23,69 @@ func TestAddition_Calculate(t *testing.T) {
 			}
 		})
 	}
+}
+func TestSubtraction_Calculate(t *testing.T) {
+	tests := []struct {
+		a, b, want int
+	}{
+		{a: 0, b: 0, want: 0},
+		{a: 0, b: 1, want: -1},
+		{a: 1, b: 1, want: 0},
+		{a: 5, b: 3, want: 2},
+	}
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("%d - %d = %d", tt.a, tt.b, tt.want), func(t *testing.T) {
+			subtraction := &Subtraction{}
+			got := subtraction.Calculate(tt.a, tt.b)
+			if got != tt.want {
+				t.Errorf("Calculate() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+func TestMultiplication_Calculate(t *testing.T) {
+	tests := []struct {
+		a, b, want int
+	}{
+		{a: 0, b: 0, want: 0},
+		{a: 0, b: 1, want: 0},
+		{a: 1, b: 1, want: 1},
+		{a: 5, b: 3, want: 15},
+	}
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("%d + %d = %d", tt.a, tt.b, tt.want), func(t *testing.T) {
+			multiplication := &Multiplication{}
+			got := multiplication.Calculate(tt.a, tt.b)
+			if got != tt.want {
+				t.Errorf("Calculate() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+func TestDivision_Calculate(t *testing.T) {
+	tests := []struct {
+		a, b, want int
+	}{
+		{a: 0, b: 1, want: 0},
+		{a: 1, b: 1, want: 1},
+		{a: 5, b: 3, want: 1},
+	}
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("%d + %d = %d", tt.a, tt.b, tt.want), func(t *testing.T) {
+			division := &Division{}
+			got := division.Calculate(tt.a, tt.b)
+			if got != tt.want {
+				t.Errorf("Calculate() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+func TestDivision_ByZeroPanics(t *testing.T) {
+	division := &Division{}
+	defer func() {
+		if r := recover(); r == nil {
+			t.Errorf("Division by zero did not panic")
+		}
+	}()
+	division.Calculate(1, 0)
 }
